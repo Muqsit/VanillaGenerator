ef0a51065e11fe8ab61898ca5a85445577549a86
MrHoller <35010313+MrHoller@users.noreply.github.com>
2021-10-11 20:15:50 +0300

    Fixing

--- src/muqsit/vanillagenerator/Loader.php
+++ src/muqsit/vanillagenerator/Loader.php
@@ -13,7 +13,7 @@ final class Loader extends PluginBase{
 
 	public function onLoad() : void{
 		$generator_manager = GeneratorManager::getInstance();
-		$generator_manager->addGenerator(NetherGenerator::class, "vanilla_nether");
-		$generator_manager->addGenerator(OverworldGenerator::class, "vanilla_overworld");
+		$generator_manager->addGenerator(NetherGenerator::class, "vanilla_nether", fn() => null);
+		$generator_manager->addGenerator(OverworldGenerator::class, "vanilla_overworld", fn() => null);
 	}
 }
